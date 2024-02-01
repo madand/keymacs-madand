4688b3cc86bb6e999ec5e74c4d2beafaeb70e261
madand <dev@madand.net>
2024-02-01 23:53:51 +0200

    Fix install script flipped files

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ X11_KBD_SYMBOLS_DIR=/usr/share/X11/xkb/symbols/
 set -xe
 
 test -d "${CONSOLE_KBD_MAPS_DIR}" \
-    && cp ./madand "${CONSOLE_KBD_MAPS_DIR}" \
+    && cp ./keymacs-madand.map "${CONSOLE_KBD_MAPS_DIR}" \
         || echo "Directory not found: ${CONSOLE_KBD_MAPS_DIR}"
 
 test -d "${X11_KBD_SYMBOLS_DIR}" \
-    && cp ./keymacs-madand.map "${X11_KBD_SYMBOLS_DIR}" \
+    && cp ./madand "${X11_KBD_SYMBOLS_DIR}" \
         || echo "Directory not found: ${X11_KBD_SYMBOLS_DIR}"
